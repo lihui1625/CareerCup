37db833bc5a753d9b8d03c8dfd81c2b0add6d263
lihui1625 <lihui1625@163.com>
2015-10-27 14:03:02 +0800

    add

--- src/zcy04/recursion/dynamic/Exam_060.java
+++ src/zcy04/recursion/dynamic/Exam_060.java
@@ -2,8 +2,37 @@ package zcy04.recursion.dynamic;
 
 public class Exam_060 {
 
+	static public void hano(int num) {
+		String from = "FROM";
+		String buffer = "BUFFER";
+		String to = "TO";
+		move(num, from, buffer, to);
+	}
+
+	static private void move(int num, String from, String buffer, String to) {
+		if (num <= 0) {
+			return;
+		}
+		move(num - 1, from, to, buffer);
+		moveStep(num, from, to);
+		move(num - 1, buffer, from, to);
+
+	}
+
+	static private void moveStep(int num, String s1, String s2) {
+		System.out.println(num + ":" + s1 + " --> " + s2);
+
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("-------" + 1 + "------");
+		hano(1);
+		System.out.println("-------" + 2 + "------");
+		hano(2);
+		System.out.println("-------" + 3 + "------");
+		hano(3);
+		System.out.println("-------" + 4 + "------");
+		hano(4);
 
 	}
 
